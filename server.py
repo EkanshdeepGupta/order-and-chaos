0362b20d53061d3ce57f093e04eb38ac4a536525
Ekanshdeep Gupta <ekanshdeep_gupta@yahoo.co.in>
2020-09-17 03:27:58 +0530

    o

--- server.py
+++ server.py
@@ -270,5 +270,4 @@ def serverCleanUp():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', )
     socketio.run(app, port=port)
