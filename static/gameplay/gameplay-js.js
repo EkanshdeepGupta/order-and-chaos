19825b71e434fbce08c199045b03f62835a5340a
Ekanshdeep Gupta <ekanshdeep_gupta@yahoo.co.in>
2020-09-17 04:13:38 +0530

    o

--- static/gameplay/gameplay-js.js
+++ static/gameplay/gameplay-js.js
@@ -7,7 +7,7 @@ function jsonData() { // Returns identifying state info
 $(document).ready(function(){
 
     var protocol = window.location.protocol;
-    socket = io(protocol + '//' document.domain + ':' + location.port); //'http://' + document.domain + ':' + location.port
+    socket = io(protocol + '//' + document.domain + ':' + location.port); //'http://' + document.domain + ':' + location.port
     socket.on('connect', function() {
         socket.emit('room', jsonData());
     });
